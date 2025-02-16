a94272df33ff189ec673713188e8236384cab40d
Jonathan Tyler Weber <jonathantweber@gmail.com>
2025-02-16 20:39:19 +0800

    done

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,35 @@
-import './style.css'
-import ReactDOM from 'react-dom/client'
-import { Canvas } from '@react-three/fiber'
-import Experience from './Experience.jsx'
+import "./style.css";
+import ReactDOM from "react-dom/client";
+import { Canvas } from "@react-three/fiber";
+import Experience from "./Experience.jsx";
+// import * as THREE from "three";
 
-const root = ReactDOM.createRoot(document.querySelector('#root'))
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+// const created = ({ gl }) => {
+//   console.log(gl);
+//   gl.setClearColor("#0000ff", 1);
+// };
+
+// const created = ({ scene }) => {
+//   console.log(scene);
+//   scene.background = new THREE.Color("skyblue");
+// };
 
 root.render(
-    <Canvas
-        camera={ {
-            fov: 45,
-            near: 0.1,
-            far: 200,
-            position: [ - 4, 3, 6 ]
-        } }
-    >
-        <Experience />
-    </Canvas>
-)
+  <Canvas
+    shadows={false}
+    camera={{
+      fov: 45,
+      near: 0.1,
+      far: 200,
+      position: [-4, 3, 6],
+    }}
+    // onCreated={created}
+  >
+    {/* can also put in experience as long as direct parent is scene */}
+    <color args={["ivory"]} attach="background" />
+
+    <Experience />
+  </Canvas>
+);
